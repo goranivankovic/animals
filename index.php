85522f8860393a836ffdc75cbff229c66e331b1e
goranivankovic <61731706+goranivankovic@users.noreply.github.com>
2020-10-11 17:05:23 +0200

    Add files via upload
    Animals Site

---
+++ index.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" 
+    integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <nav class="navbar bg-dark">
+      <a class="navbar-brand  text-danger" href="https://goranivankovic.github.io/MySite/">gogaDesign</a>
+      <ul class="navbar-nav">
+
+        <li class="nav-item"><a class="nav-link active" href="https://animals.sandiegozoo.org/">Home</a></li> 
+
+
+      </ul>
+    </nav>
+    <br>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <h1 class="text-center">This  are my favourite animals</h1>
+
+
+            </div>
+        </div>
+    </div>
+    <br>
+
+    <br>
+    <div class="container">
+       
+
+        <div class="row" style="justify-content: space-around;">
+            <div class="col-md-5 jedan">
+                <div class="card border-success" >
+                    <img src="images/lion2.jpg" class="card-img-top" alt="lion" width="100%" height="60%">
+                    <div class="card-body">
+                        <div class="card-titel"><h1 class="text-left">Lion</h1> </div>
+                        <div class="card-text"><h4>This is male lion at age of 5 years.</h4> 
+                        </div>
+                        <br>
+                        <a href="https://www.nationalgeographic.com/animals/mammals/a/african-lion/" class="btn btn-primary btn-md">Learn more</a>
+                    </div>
+                    
+                </div>
+            </div>
+            <br></br>
+            
+            <div class="col-md-5">
+                <div class="card border-success">
+                    <img src="images/leopard.jpg" class="card-img-top" alt="liones" width="100%" height="50%">
+                    <div class="card-body">
+                        <div class="card-titel"><h1 class="text-right">Leopard</h1> </div>
+                        <div class="card-text"><h4>This is leopard at age of 5 years.</h4> 
+                        </div>
+                        <br>
+                        <a href="https://www.nationalgeographic.com/animals/mammals/l/leopard/" class="btn btn-primary btn-md">Learn more</a>
+                    </div>
+                </div>
+            </div>
+            </div>
+        
+
+       
+    </div>
+    <br>
+    <div class="jumbotron">
+        <h1 class="text-primary">Lions</h1>
+        <br>
+        <p  class="text-left">
+            Lions are famous for their loud roar which can be heard up to 5 miles away. <br>
+            They can make such a loud roar because the cartilage in their throats has turned into bone.<br>
+            They tend to roar more at night.<br>
+            The lion is taller than the tiger, but doesn't weigh as much.
+            The lion's main competitor for prey in Africa is the spotted hyena.
+            Even though the female lions do the hunting, the male lion often gets to eat first.
+            They are excellent swimmers.
+            Lions will live about 15 years in the wild.
+
+        </p>
+    </div>
+    
+ 
+
+    <div class="jumbotron">
+        <h1 class="text-primary">Leopards</h1>
+        <p class="text-left">
+           The leopard is so strong and comfortable in trees that it often hauls its kills into the branches. <br>
+           By dragging the bodies of large animals aloft it hopes to keep them safe from scavengers such as hyenas.<br>
+           Leopards can also hunt from trees, where their spotted coats allowthem to blend with the leaves <br> until they spring with a deadly pounce.<br>
+           These nocturnal predators also stalk antelope, deer, and pigs by stealthy movements in the tall grass. <br>
+           When human settlements are present, leopards often attack dogs and, occasionally, people.<br>
+           Leopards are strong swimmers and very much at home in the water, where they sometimes eat fish or crabs.
+
+
+        </p>
+
+    </div>
+    <br>
+   
+   
+    
+  <div class="container" >
+      <div class="row">
+          <div class="col">
+              <h3 class="fo">powered by: <span id="ime" class="text-primary">gogaDesign</span></h3>
+          </div>
+      </div>
+  </div>   
+ 
+    
+</body>
+</html>
